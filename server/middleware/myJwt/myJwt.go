5bfa8788354c369c233e0004edec15233122ecf0
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-04-28 13:26:49 +0500

    Add: jwt packageoutline

---
+++ server/middleware/myJwt/myJwt.go
@@ -0,0 +1,55 @@
+
+
+
+package myjwt;
+
+
+
+const (
+	privKeyPath = "keys/app.rsa"
+    pubKeyPath = "keys/app.rsa.pub"
+)
+
+
+func InitJWT()error{
+
+}
+
+func CreateNewTokens()(){
+
+}
+
+func CheckAndRefreshTokens()(){
+
+}
+
+func createAuthTokenString()(){
+
+}
+
+
+func createRefreshTokenString()(){
+
+}
+
+func updateRefreshTokenExp()(){
+
+}
+
+func updateAuthTokenString() error{
+
+}
+
+
+func RevokeRefreshToken()error{
+
+}
+
+
+func updateRefreshTokenCsrf()(){
+
+}
+
+func GrabUUID()(){
+
+}
